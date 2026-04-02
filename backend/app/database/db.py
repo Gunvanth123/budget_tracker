1e095dc3fb57c43b95f3f28da42b3cd264c3098e
Gunvanth123 <guna.konathala123@gmail.com>
2026-04-02 19:27:10 +0530

    changes env

--- backend/app/database/db.py
+++ backend/app/database/db.py
@@ -15,7 +15,11 @@ DATABASE_URL = os.getenv(
 if DATABASE_URL.startswith("postgres://"):
     DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
 
-engine = create_engine(DATABASE_URL, pool_pre_ping=True)
+engine = create_engine(
+    DATABASE_URL,
+    pool_pre_ping=True,
+    connect_args={"sslmode": "require"}
+)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
